3ddac08fbe94780b81cd554d918f12ef5abc8b9d
angel <angel@HP-ANGEL>
2018-02-13 23:09:00 -0600

    Creación de Jugadores manualmente

---
+++ Laboratorio1/Laboratorio1/Controllers/JugadorController.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Laboratorio1.DBContext;
+
+namespace Laboratorio1.Controllers
+{
+    public class JugadorController : Controller
+    {
+        public DefaultConnection db = DefaultConnection.getInstance;
+
+        // GET: Jugador
+        public ActionResult Index()
+        {
+            return View(db.Jugadores.ToList());
+        }
+
+        // GET: Jugador/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Jugador/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Jugador/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include ="Nombre,Apellido,Club,Posicion,Salario")]Models.Jugador jugador)
+        {
+            if (ModelState.IsValid)
+            {
+                jugador.JugadorID = ++db.IdActual;
+                db.Jugadores.Add(jugador);
+                return RedirectToAction("Index");
+            }
+            return View(jugador);
+        }
+
+        // GET: Jugador/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Jugador/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Jugador/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Jugador/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Laboratorio1/Laboratorio1/Models/Jugador.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Laboratorio1.Models
+{
+    public class Jugador
+    {
+        public int JugadorID { get; set; }
+        public string Apellido { get; set; }
+        public string Nombre { get; set; }
+        public string Club { get; set; }
+        public string Posicion { get; set; }
+        public int Salario { get; set; }
+    }
+}
